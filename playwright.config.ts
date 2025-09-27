7261f22d51ede54798b400cbf393130003885ca0
Hi-kue <lofi.audit@gmail.com>
2025-09-27 00:26:22 -0400

    ref: major refactor to codebase, svelte to next

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,8 +2,17 @@ import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   webServer: {
-    command: "npm run build && npm run preview",
-    port: 4173,
+    command: "npm run dev",
+    url: "http://localhost:3000",
+    port: 3000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI
   },
   testDir: "e2e",
+  reporter: "list",
+  use: {
+    baseURL: "http://localhost:3000",
+    trace: "on-first-retry",
+  },
+  timeout: process.env.CI ? 60000 : 30000
 });
